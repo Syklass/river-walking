00e1e6bcf6d78a6b52c7b688bf7a942c338120a9
Syklass <Syklass@gmail.com>
2022-09-09 12:59:47 +0300

    Добавил материал юи

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import DehazeIcon from '@mui/icons-material/Dehaze';
 
 function Header() {
     return (  
-        <div>
-            орп
+        <div className="padding-x py-4">
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col text-center">
+                    <h1 className="font-semibold text-4xl uppercase">Трошин Вячеслав</h1>
+                    <h2 className="font-semibold text-xl uppercase">Junior Frontend Developer</h2>
+                </div>
+                <div className="hidden lg:block">
+                    <nav className="flex">
+                        <a className="mx-2 hover:text-stone-400 transition-all cursor-pointer select-none font-semibold text-xl"
+                        onClick={()=>{alert("Раздел временно не готов")}}>Умения</a>
+                        <a className="mx-2 hover:text-stone-400 transition-all cursor-pointer select-none font-semibold text-xl"
+                        onClick={()=>{alert("Раздел временно не готов")}}>Опыт работы</a>
+                        <a className="mx-2 hover:text-stone-400 transition-all cursor-pointer select-none font-semibold text-xl"
+                        onClick={()=>{alert("Раздел временно не готов")}}>Обо мне</a>
+                        <a className="mx-2 hover:text-stone-400 transition-all cursor-pointer select-none font-semibold text-xl"
+                        onClick={()=>{alert("Раздел временно не готов")}}>Почему я?</a>
+                    </nav>
+                </div>
+                <div className="block lg:hidden">
+                    <DehazeIcon className="cursor-pointer" onClick={()=>{alert("123")}}/>
+                    <CloseIcon className="cursor-pointer" />
+                </div>
+            </div>
         </div>
     );
 }
